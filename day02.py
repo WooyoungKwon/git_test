106ade5076c11aea2f65783072c1a11de23aa4db
wooyoung <rnjsdndud00@naver.com>
2023-01-13 15:26:34 +0900

    random

--- day02.py
+++ day02.py
@@ -1,4 +1,11 @@
-print(2 ** 4)
-print(divmod(9, 5))
-print(pow(2, 5))
-print((-5) ** 2)
+# Chap 4 if
+import random
+
+limits = 20
+tweets = "pass" * random.randint(1, 10)
+diff = limits - len(tweets)
+# if diff := limits - len(tweets) >= 0:
+if diff >= 0:
+    print(tweets)
+else:
+    print(f'글자 수 {abs(diff)}초과')
