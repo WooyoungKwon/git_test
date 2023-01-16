892ebce626ff84e579c139d6a761648be0026e54
wooyoung <rnjsdndud00@naver.com>
2023-01-16 16:31:56 +0900

    day03 Homework 5.6

--- day03.py
+++ day03.py
@@ -1,13 +1,6 @@
-# salutation = 'Mr.'
-# name = 'Kwon'
-# product = 'Gun', verbed = ' ', room = 'house', animals = 'tiger', amount = '1,000$', spokesman = 'Kim', job_title = 'owner', percent = '70'
-
-letter = 'Dear {salutation} {name}, \n' \
-         'Thank you for your letter. \n' \
-         'We are sorry that our {product} {verbed} in your {room}. \n' \
-         'Please note that it should never be used in a {room}, especially near nay {animals}. \n' \
-         'Send us your receipt and {amount} for shipping and handling. \n' \
-         'We will send you another {product} that, in our tests, is {percent}% less likely to have {verbed}. \n' \
-         'Thank you for your support. Sincerely, {spokesman} {job_title} \n'\
-         .format(salutation = 'Mr', name = 'Kwon', product = 'Gun', verbed = ' ', room = 'house', animals = 'tiger', amount = '1,000$', spokesman = 'Kim', job_title = 'owner', percent = '70')
-print(letter)
+du = 'Ducky McDuckface'
+go = 'Gourdy McGourdface'
+sp = 'Spitzy McSpitzface'
+print('an English Submarine 이름은 %s로 지어졌다.'%du)
+print('an Australian racehorse 이름은 %s로 지어졌다.'%go)
+print('a Swedish train 이름은 %s로 지어졌다.' % sp)
