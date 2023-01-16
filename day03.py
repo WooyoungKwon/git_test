f30b26fa149d1786fe7cc504fe4212f765ae43d5
wooyoung <rnjsdndud00@naver.com>
2023-01-16 16:44:38 +0900

    day03 Homework 6.2

--- day03.py
+++ day03.py
@@ -1,6 +1,12 @@
-du = 'Ducky McDuckface'
-go = 'Gourdy McGourdface'
-sp = 'Spitzy McSpitzface'
-print(f'an English Submarine 이름은 {du}로 지어졌다. \n'
-      f'an Australian racehorse 이름은 {go}로 지어졌다. \n'
-      f'a Swedish train 이름은 {sp}로 지어졌다.')
+guess_me = 7
+number = 1
+while True:
+    if number < guess_me:
+        print('too low')
+    elif number == guess_me:
+        print('found it!')
+        break
+    else:
+        print('oops')
+        break
+    number += 1
