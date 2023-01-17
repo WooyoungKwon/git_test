8bd13cbc046766dbdf9a2d3eb62de12a00b9fcc2
wooyoung <rnjsdndud00@naver.com>
2023-01-17 18:19:26 +0900

    day04_8.13-8.14

--- day04.py
+++ day04.py
@@ -1,5 +1,12 @@
-squares = {k: k**2 for k in range(10)}
-odd = {num for num in squares if num % 2 == 1}
-generator = (ge for ge in odd)
-for i in generator:
-    print(i)
+#8.13
+ZipKey = ('optimist', 'pessimist', 'troll')
+ZipValue = ('The glass is half full', 'The glass is half empty', 'How did you get a glass?')
+
+aa = {k: v for k, v in zip(ZipKey, ZipValue)}
+print(aa)
+
+#8.14
+titles = ['Creature of Habit', 'Crewel Fate']
+plots = ['A nun turns into a monster', 'A haunted yarn shop']
+movies = dict(zip(titles, plots))
+print(movies)
