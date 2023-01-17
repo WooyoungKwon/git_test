18494408e3b8bf3d2f1dd6521568d23cdb3c49f8
wooyoung <rnjsdndud00@naver.com>
2023-01-17 17:49:28 +0900

    day04_8.6-8.9

--- day04.py
+++ day04.py
@@ -1,11 +1,8 @@
-e2f = {
-    'dog': 'chien',
-    'cat': 'chat',
-    'walrus': 'morse'
+life = {
+    'animals': {'cats': 'Henri', 'octopi': 'Grumpy', 'emus': 'Lucy'},
+    'plants': ' ',
+    'other': ' '
 }
-print(e2f['walrus'])
-
-f2e = {v: k for k, v in e2f.items()}
-print(f2e['chien'])
-
-[print(key) for key in e2f.keys()]
+[print(key) for key in life]
+[print(key) for key in life['animals'].keys()]
+print(life['animals']['cats'])
