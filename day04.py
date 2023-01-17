7aeb6cc9aaab2fa575299d2308f26b82425d4623
wooyoung <rnjsdndud00@naver.com>
2023-01-17 16:33:17 +0900

    day04_7.11

--- day04.py
+++ day04.py
@@ -1,2 +1,17 @@
-number = [numbers for numbers in range(10) if numbers % 2 == 0]
-print(number)
+start1 = ["fee", 'fie', 'foe']
+rhymes = [
+    ('flop', 'get a mop'),
+    ('fope', 'turn the rope'),
+    ('fa', 'get your ma'),
+    ('fudge', 'call the judge'),
+    ('fat', 'pet the cat'),
+    ('fog', 'walk the dog'),
+    ('fun', "say we're done"),
+]
+start2 = 'Someone better'
+for i in range(3):
+    start1[i] = start1[i].title()
+
+start1 = '! '.join(start1)
+print(start1 + '! ' + (rhymes[0][0].title() + '!'))
+print(start2, rhymes[0][1] + '.')
