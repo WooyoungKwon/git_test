469209b2941204ad2f20847dd759e8b6508643a7
wooyoung <rnjsdndud00@naver.com>
2023-01-17 16:17:04 +0900

    day04

--- day03.py
+++ day03.py
@@ -1,4 +1,18 @@
-du = 'Ducky McDuckface'
-go = 'Gourdy McGourdface'
-sp = 'Spitzy McSpitzface'
-print('an English Submarine 이름은 %s로 지어졌다. \nan Australian racehorse 이름은 %s로 지어졌다. \na Swedish train 이름은 %s로 지어졌다.' % (sp, du, go))
+count = int(input())
+j=1
+score = 0
+for i in range(count):
+    arr = list(input())
+    print(arr[0])
+    if arr[i] == 'O':
+        score += 1
+        print('1')
+        for j in range(count-1):
+            print('1.5')
+            if arr[i+j] == 'X':
+                print('2')
+                break
+            elif arr[i+j] == 'O':
+                print('3')
+                score += j
+print(score)
---
+++ day04.py
@@ -0,0 +1,4 @@
+surprise = ['Groucho', 'Chico', 'Harpo']
+HarpoLower = surprise[-1].lower()
+ReverseHarpo = HarpoLower[::-1]
+ReverseHarpo_title = ReverseHarpo.title()
