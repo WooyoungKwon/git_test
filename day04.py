3e492fc2e2ab5d9a166345838db2cd24cbc90387
wooyoung <rnjsdndud00@naver.com>
2023-01-17 17:06:24 +0900

    day04_8.1-8.5

--- day04.py
+++ day04.py
@@ -1,17 +1,11 @@
-start1 = ["fee", 'fie', 'foe']
-rhymes = [
-    ('flop', 'get a mop'),
-    ('fope', 'turn the rope'),
-    ('fa', 'get your ma'),
-    ('fudge', 'call the judge'),
-    ('fat', 'pet the cat'),
-    ('fog', 'walk the dog'),
-    ('fun', "say we're done"),
-]
-start2 = 'Someone better'
-for i in range(3):
-    start1[i] = start1[i].title()
+e2f = {
+    'dog': 'chien',
+    'cat': 'chat',
+    'walrus': 'morse'
+}
+print(e2f['walrus'])
 
-start1 = '! '.join(start1)
-print(start1 + '! ' + (rhymes[0][0].title() + '!'))
-print(start2, rhymes[0][1] + '.')
+f2e = {v: k for k, v in e2f.items()}
+print(f2e['chien'])
+
+[print(key) for key in e2f.keys()]
