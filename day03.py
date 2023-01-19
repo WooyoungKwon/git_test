6d5c189306bc840516bc7e73fb544dd7e22e4d5b
wooyoung <rnjsdndud00@naver.com>
2023-01-19 17:04:33 +0900

    day06_9.3

--- day03.py
+++ day03.py
@@ -1,18 +1,15 @@
-count = int(input())
-j=1
-score = 0
-for i in range(count):
-    arr = list(input())
-    print(arr[0])
-    if arr[i] == 'O':
-        score += 1
-        print('1')
-        for j in range(count-1):
-            print('1.5')
-            if arr[i+j] == 'X':
-                print('2')
-                break
-            elif arr[i+j] == 'O':
-                print('3')
-                score += j
-print(score)
+def start(func):
+    def func2():
+        print('start')
+        func()
+        print('end')
+
+    return func2()
+
+
+def temp():
+    pass
+
+
+start(temp)
+
--- day05.py
+++ day05.py
@@ -1,9 +1,23 @@
-def good():
-    """
+def selfNumber(n):
+    all_number = {i for i in range(n)} # 1부터 n까지의 set 생성
+    con_number = set() # 생성자가 존재하는 정수가 담길 set 생성
 
-    :return: list
-    """
-    return ['Harry', 'Ron', 'Hermione']
+    for i in all_number: # all_number의 길이를 재서 돌아가게 하고 i에 all_number의 객체가 들어가는 부분은 따로 구현
+        temp = []
+        # 리스트로 변환해서 자릿수 별로 나누기
+        for j in str(i):
+            temp.append(j)
+            # 계산을 위해 다시 정수로 변환
+            for k in range(len(temp)):
+                temp[k] = int(temp[k])
+        print(temp)
+        hap = i + int(temp[0]) + int(temp[1]) # 한 자릿 수는 계산이 불가능 하기 때문에 인덱스 에러 발생
+        # print(f'temp{int(temp[0])}')
 
+        con_number.add(hap)
+    return print(f'이건가{all_number - con_number}')
 
-print(good())
+selfNumber(100)
+# aa = {1, 2, 3, 4}
+# bb = {2, 3, 4, 6}
+# print(aa & bb)
