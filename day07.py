1f6215e98641abb1ad2b40f6d83b191265f5cebe
wooyoung <rnjsdndud00@naver.com>
2023-01-20 18:44:42 +0900

    10.5

--- day07.py
+++ day07.py
@@ -1,9 +1,14 @@
-#10.4
+# 10.5
 class Element:
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
 
+    def __str__(self):
+        # print(class)
+        return self.name, self.symbol, self.number
+
 
 no1 = Element('Hydrogen', 'H', 1)
+print(vars(no1))
