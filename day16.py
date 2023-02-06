5b7caf7bb9b63044e157ea3dc0b4e3e5f1151f5d
wooyoung <rnjsdndud00@naver.com>
2023-02-06 22:06:23 +0900

    SelfStudy5.2

--- day16.py
+++ day16.py
@@ -1,5 +1,9 @@
-#5-1
-class Node:
+#5.2
+import random
+
+
+## 클래스와 함수 선언 부분 ##
+class Node():
     def __init__(self):
         self.data = None
         self.link = None
@@ -7,6 +11,8 @@ class Node:
 
 def printNodes(start):
     current = start
+    if current.link == None:
+        return
     print(current.data, end=' ')
     while current.link != start:
         current = current.link
@@ -14,81 +20,46 @@ def printNodes(start):
     print()
 
 
-def insertNode(findData, insertData):
+def countOddEven():
     global memory, head, current, pre
 
-    if head.data == findData:  # 첫 번째 노드 삽입
-        node = Node()
-        node.data = insertData
-        node.link = head
-        last = head
-        while last.link != head:
-            last = last.link
-        last.link = node
-        head = node
-        return
+    odd, even = 0, 0
+    # if head == None:
+    #     return False
     current = head
-    while current.link != head:  # 중간 노드 삽입
-        pre = current
+    while True:
+        if current.data > 0:  # 양수
+            even += 1
+        elif current.data < 0:  # 음수
+            odd += 1
+        if current.link == head:
+            break
         current = current.link
-        if current.data == findData:
-            node = Node()
-            node.data = insertData
-            node.link = pre.link
-            pre.link = node
-            return
-
-    node = Node()
-    node.data = insertData
-    current.link = node
-    node.link = head
+    return odd, even
 
 
-def deleteNode(deleteData):
-    global memory, head, current, pre
-
-    if head.data == deleteData:
-        current = head  # head 노드(첫 번째 노드)를 current 노드에 저장
-        head = head.link  # head를 head 다음 노드로 변경
-        last = head
-        while last.link != current:  # 마지막 노드까지 이동
-            last = last.link
-        last.link = head  # 마지막 노드를 head랑 연결
-        del current
-        return
+def makeZeroNumber():  # odd = 홀수, even = 짝수
+    # if odd > even:
+    #     reminder = 1
+    # else:
+    #     reminder = 0
 
     current = head
     while current.link != head:
-        pre = current
+        current.data *= -1
         current = current.link
-        if current.data == deleteData:
-            pre.link = current.link
-            del current
-            return
 
 
-def findNode(findData):
-    global memory, head, current, pre
-
-    current = head
-    if current.data == findData:
-        print('# 첫 노드에서 찾음 #')
-        return current
-    while current.link != head:
-        current = current.link
-        if current.data == findData:
-            print('# 중간 노드에서 찾음 #')
-            return current
-    print('# 찾는 노드가 없음 #')
-    return Node()
-
-
-# 전역 변수
+## 전역 변수 선언 부분 ##
 memory = []
-head, current, node = None, None, None
-dataArray = ['다현', '정연', '쯔위', '사나', '지효']
-# 메인 코드 실행
+head, current, pre = None, None, None
+
+## 메인 코드 부분 ##
 if __name__ == "__main__":
+
+    dataArray = []
+    for _ in range(7):
+        dataArray.append(random.randint(-100, 100))
 
     node = Node()
     node.data = dataArray[0]
@@ -97,16 +68,15 @@ if __name__ == "__main__":
     memory.append(node)
 
     for data in dataArray[1:]:
-        pre = node  # data 갯수가 몇 개인지 모르기 때문에, 같은 변수명(node)를 사용한다.
+        pre = node
         node = Node()
         node.data = data
         pre.link = node
         node.link = head
         memory.append(node)
 
-    fNode = findNode('다현')
-    print(fNode.data)
-    fNode = findNode('쯔위')
-    print(fNode.data)
-    fNode = findNode('재남')
-    print(fNode.data)
+    oddCount, evenCount = countOddEven()
+    printNodes(head)
+    print(f"양수 --> {evenCount} 음수 --> {oddCount}")
+    makeZeroNumber()
+    printNodes(head)
